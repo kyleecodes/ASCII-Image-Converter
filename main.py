f366704a131285dc3fe45e99b8e114634a189e77
kyleecodes <kfieldsinmd@gmail.com>
2021-01-29 16:06:07 -0500

    Can now adjust for any height or width image

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import PIL.Image
+import os
+import sys
 
 ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
 
 
-def resize(image, new_width=100):
-    height, width = image.size
-    ratio = height / width
+#  set width and then create new height based on ratio
+# setting width is necessary for ensuring appropriate widths for your display
+def resize(image, new_width=50):
+    # (size() returns 2-tuple width height of pixels)
+    width, height = image.size
+    print(f"Image width, height: {image.size}")
+    # ratio depends on image proportions
+    if height > width:
+        ratio = width / height
+        print(f"Height greater ratio: {ratio}")
+    elif height < width:
+        ratio = height / width
+        print(f"Width greater ratio: {ratio}")
+    # calculate new height based on new width and ratio
     new_height = int(new_width * ratio)
+    # print(f"the ratio: {ratio}")
     return image.resize((new_width, new_height))
 
 
@@ -16,28 +30,49 @@ def to_greyscale(image):
 
 def pixel_to_ascii(image):
     pixels = image.getdata()
-    ascii_str = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    print(f"Pixels data: {pixels}")
+    count = 0
+    for pixel in pixels:
+        count += 1
+        print(f"Count({count}) Pixel: {pixel} \n")
+    # TODO: needs comment on number of ASCII CHARS and 25 -- how to maximize symbols?
+    # might want to abstract this out into the main function
+    # added & symbol in front and changed 25 to 17 and I have no idea why that works but it does
+    # if I add a symbol I need to -1 from int pixel index to MOVE UP LIST not add to it
+    # why is 17 the sweet spot?
+    # index depends on image argument
+    # pixel // 25 to make index an int of the ASCII CHARS list
+    # (The // operator is used for truncating division)
+    # TODO: how is this assessing light vs. dark pixels?????
+    ascii_str = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
     return ascii_str
 
 
-def main():
-    path = "test_images/dog.jpg"
-    image = PIL.Image.open(path)
-    # resize image
-    image = resize(image)
-    # convert image to greyscale image
-    greyscale_image = to_greyscale(image)
-    # convert greyscale image to ascii characters
+def print_image(greyscale_image):
     ascii_str = pixel_to_ascii(greyscale_image)
     img_width = greyscale_image.width
     ascii_str_len = len(ascii_str)
+    # official list printed
     ascii_img = ""
     # Split the string based on width  of the image
     for i in range(0, ascii_str_len, img_width):
+        # width line by line of ascii characters printed to official list
         ascii_img += ascii_str[i:i + img_width] + "\n"
     # save the string to a file
     with open("ascii_image.txt", "w") as f:
         f.write(ascii_img)
 
 
-main()
+def main(path):
+    image = PIL.Image.open(path)
+    # print(image.format, image.size, image.mode)
+    image = resize(image)
+    # print(image.format, image.size, image.mode)
+    greyscale_image = to_greyscale(image)
+    # print(greyscale_image.format, greyscale_image.size, greyscale_image.mode)
+    print_image(greyscale_image)
+
+
+if __name__ == '__main__':
+    path = "test_images/bw.jpg"
+    main(path)
